a38c3eaa88b48b67afd00292ad2bccd55c2ea110
bishwasgiri <giribishwas4@gmail.com>
2024-12-30 14:27:19 +0545

    slight change

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="mt-10">
-      <div className="relative w-[100vw] bg-red-700">
+      <div className="relative w-[100vw">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3533.47183124352!2d85.34724712604957!3d27.67180832705693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19f9807e234f%3A0xab7b3eaf6f7d51d0!2sLaligurans%20Batika%20School!5e0!3m2!1sen!2snp!4v1735541319842!5m2!1sen!2snp"
           height="450"
